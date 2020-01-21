f11d7091a4dff092335f657f8bdfa0d91ea42d63
Hariluk Fuklum <Hariluk.F@DCS.CORP>
2020-01-21 15:24:44 +0700

    first build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'authentication', pathMatch: 'full' },
+  {
+    path: 'authentication',
+    loadChildren: () => import('./authentication/page/authentication/authentication.module').then( m => m.AuthenticationPageModule)
+  },
 ];
 
 @NgModule({
